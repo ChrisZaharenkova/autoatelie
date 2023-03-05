f655dc44b13aaddde844689e34fb9d309915dc87
ChrisZaharenkova <kristina.zaharenkova00@mail.ru>
2023-03-05 20:10:23 +0300

    scroll

--- js/script.js
+++ js/script.js
@@ -6,3 +6,17 @@ $(function(){
     });
     
   });
+  const anchors = document.querySelectorAll('a.scroll-to')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href')
+    
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
