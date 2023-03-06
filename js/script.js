08cfcd25421d2b6cc00e8d4adbc8d1cc664a96ac
ChrisZaharenkova <kristina.zaharenkova00@mail.ru>
2023-03-06 15:34:04 +0300

    form

--- js/script.js
+++ js/script.js
@@ -20,3 +20,81 @@ for (let anchor of anchors) {
     })
   })
 }
+const closeBlock = document.querySelector('.block__close')
+const block = document.querySelector('.block')
+const button = document.querySelector('.fullscreen__button')
+
+button.addEventListener('click',function(){
+    console.log("click")
+    block.classList.add('open');
+})
+closeBlock.addEventListener('click',function(){
+    console.log('close')
+    block.classList.remove('open')
+})
+console.log('Init!');
+
+
+// inputmask
+const form = document.querySelector('.form');
+const telSelector = form.querySelector('input[type="tel"]');
+const inputMask = new Inputmask('+7 (999) 999-99-99');
+inputMask.mask(telSelector);
+
+
+new window.JustValidate('.form', {
+  rules: {
+    email:{
+      required: false,   
+    },
+    name:{
+      required:false,
+    },
+    tel: {
+      required: false,
+      /*
+      function: () => {
+        const phone = telSelector.inputmask.unmaskedvalue();
+        return Number(phone) && phone.length === 10;
+      }*/
+    }
+  },
+  colorWrong: '#ff0f0f',
+  messages: {
+    name: {
+      required: 'Введите имя',
+      minLength: 'Введите 3 и более символов',
+      maxLength: 'Запрещено вводить более 15 символов'
+      
+    },
+    email: {
+      email: 'Введите корректный email',
+      required: 'Введите email'
+    },
+    tel: {
+      required: 'Введите телефон',
+     
+    }
+  },
+  
+  submitHandler: function(thisForm) {
+    let formData = new FormData(thisForm);
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          console.log('Отправлено');
+          const apply = document.querySelector('.apply');
+          apply.classList.add('open'); 
+        }
+      }
+    }
+
+    xhr.open('POST', 'mail.php', true);
+    xhr.send(formData);
+
+    thisForm.reset();
+  }
+});
